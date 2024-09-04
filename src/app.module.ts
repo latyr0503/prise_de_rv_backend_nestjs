41641b7d7adda76dcba009ce598f270c160ecd2d
latyr0503 <latyr0503@gmail.com>
2024-09-04 13:57:07 +0000

    add file .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,20 @@ import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { Patient } from './patient/patient.entity';
 import { RendezVous } from './rendezvous/rendezvous.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'prise_de_rendezvous',
-      entities: [],
+      type: process.env.DB_TYPE as 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Patient, RendezVous],
       autoLoadEntities: true,
       synchronize: true,
     }),
